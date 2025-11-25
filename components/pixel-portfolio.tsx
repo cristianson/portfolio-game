312d6d09aa127cd4736065e845de484deec638c3
Cristian <cristian.iordan2@gmail.com>
2025-11-25 11:28:43 +0100

    character redesign and xmas look

--- components/pixel-portfolio.tsx
+++ components/pixel-portfolio.tsx
@@ -16,7 +16,7 @@ interface GameState {
 }
 
 // Constants
-const PLAYER_SIZE = 48;
+const PLAYER_SIZE = 80;
 const INTERACTION_DISTANCE = 80; // Adjusted to match visual ring (Radius 80px)
 const INTERACTION_BUFFER = 10;
 
@@ -532,31 +532,81 @@ export default function PixelPortfolio() {
             Player 1
           </div>
 
-          {/* Character Sprite Placeholder */}
+          {/* Character Sprite - Pixel Human with Santa Hat */}
           <div
-            className={`w-full h-full relative transition-transform ${
+            className={`w-full h-full relative transition-transform duration-150 ${
               gameState.direction === "LEFT" ? "scale-x-[-1]" : ""
             }`}
           >
-            {/* Body */}
-            <div className="absolute bottom-0 w-8 h-10 bg-blue-600 left-2 pixel-border-sm"></div>
-            {/* Head */}
-            <div className="absolute bottom-10 w-8 h-8 bg-orange-200 left-2 pixel-border-sm">
-              {/* Eyes */}
-              <div className="absolute top-3 left-1 w-1 h-1 bg-black"></div>
-              <div className="absolute top-3 right-1 w-1 h-1 bg-black"></div>
+            {/* Santa Hat */}
+            <div className="absolute top-[-12px] left-1/2 -translate-x-1/2 w-10 h-8 z-30">
+              {/* Pom-pom - Centered on top */}
+              <div className="absolute -top-3.5 left-1/2 -translate-x-1/2 w-3 h-3 bg-white pixel-border-sm rounded-full z-40"></div>
+              {/* Red Hat Body */}
+              <div className="absolute top-0 left-1/2 -translate-x-1/2 w-8 h-6 bg-red-600 pixel-border-sm rounded-t-full"></div>
+              {/* White Trim */}
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-10 h-3 bg-white pixel-border-sm"></div>
             </div>
-            {/* Legs Animation */}
-            <div
-              className={`absolute -bottom-1 left-2 w-3 h-3 bg-black ${
-                gameState.isMoving ? "animate-pulse" : ""
-              }`}
-            ></div>
-            <div
-              className={`absolute -bottom-1 right-4 w-3 h-3 bg-black ${
-                gameState.isMoving ? "animate-pulse delay-75" : ""
-              }`}
-            ></div>
+
+            {/* Head */}
+            <div className="absolute top-2 left-1/2 -translate-x-1/2 w-8 h-6 z-20">
+              {/* Hair */}
+              <div className="absolute top-0 left-0 w-full h-2.5 bg-amber-800 pixel-border-sm"></div>
+              <div className="absolute top-2 -left-0.5 w-1 h-4 bg-amber-800"></div>
+              <div className="absolute top-2 -right-0.5 w-1 h-4 bg-amber-800"></div>
+
+              {/* Face */}
+              <div className="absolute top-2.5 left-0 w-full h-3.5 bg-orange-200 pixel-border-sm">
+                {/* Eyes */}
+                <div className="absolute top-1 left-2 w-1 h-1 bg-black"></div>
+                <div className="absolute top-1 right-2 w-1 h-1 bg-black"></div>
+                {/* Blush */}
+                <div className="absolute top-2.5 left-1.5 w-1 h-0.5 bg-pink-300/50"></div>
+                <div className="absolute top-2.5 right-1.5 w-1 h-0.5 bg-pink-300/50"></div>
+              </div>
+            </div>
+
+            {/* Body (Torso) */}
+            <div className="absolute top-8 left-1/2 -translate-x-1/2 w-7 h-8 bg-blue-600 z-10 pixel-border-sm">
+              {/* Shirt Details */}
+              <div className="absolute top-0 left-1/2 -translate-x-1/2 w-2 h-4 bg-blue-400"></div>
+            </div>
+
+            {/* Arms */}
+            <div className="absolute top-8 left-1/2 -translate-x-1/2 w-11 h-7 z-15 pointer-events-none">
+              {/* Left Arm */}
+              <div
+                className={`absolute left-[-4px] top-0 w-2 h-7 bg-blue-700 origin-top transition-transform ${
+                  gameState.isMoving ? "rotate-12 translate-y-[-1px]" : ""
+                }`}
+              >
+                <div className="absolute bottom-0 w-full h-2 bg-orange-200 border-t border-black/10"></div>
+              </div>
+              {/* Right Arm */}
+              <div
+                className={`absolute right-[-4px] top-0 w-2 h-7 bg-blue-700 origin-top transition-transform ${
+                  gameState.isMoving ? "-rotate-12 translate-y-[-1px]" : ""
+                }`}
+              >
+                <div className="absolute bottom-0 w-full h-2 bg-orange-200 border-t border-black/10"></div>
+              </div>
+            </div>
+
+            {/* Legs */}
+            <div className="absolute top-16 left-1/2 -translate-x-1/2 w-7 h-6 z-0">
+              {/* Left Leg */}
+              <div
+                className={`absolute left-0.5 top-0 w-2.5 h-full bg-white origin-top transition-all duration-150 ${
+                  gameState.isMoving ? "h-[80%] translate-y-[-1px]" : "h-full"
+                }`}
+              ></div>
+              {/* Right Leg */}
+              <div
+                className={`absolute right-0.5 top-0 w-2.5 h-full bg-white origin-top transition-all duration-150 delay-75 ${
+                  gameState.isMoving ? "h-full" : "h-full"
+                }`}
+              ></div>
+            </div>
           </div>
         </div>
       </div>
